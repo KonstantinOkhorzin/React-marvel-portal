43c1bcc5a32755b0acece78df805e730e5c3f1a9
Konstantin Okhorzin <konstantinokhorzin@gmail.com>
2021-11-29 08:33:07 +0200

    add new chars

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -3,6 +3,7 @@ class MarvelService {
 
     _apiBase = 'https://gateway.marvel.com:443/v1/public/';
     _apiKey = 'apikey=6317913af99eb03dd17f1be5e6606383';
+    _baseOffset = 210;
 
     getResourse = async (url) => {
         let res = await fetch(url);
@@ -14,8 +15,8 @@ class MarvelService {
         return await res.json();
     }
 
-    getAllCharacters = async () => {
-        const res = await this.getResourse(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    getAllCharacters = async ( offset = this._baseOffset ) => {
+        const res = await this.getResourse(`${this._apiBase}characters?limit=9&offset=${offset}&${this._apiKey}`);
         return res.data.results.map(this._transformCharacter);
     }
 
