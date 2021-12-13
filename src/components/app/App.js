0fbc1a780a9d57ba68d0fadc7936cf27288c61b4
Konstantin Okhorzin <konstantinokhorzin@gmail.com>
2021-12-13 12:26:27 +0200

    add SinglePage

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,9 +1,11 @@
 import { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import { MainPage, ComicsPage, SingleComicPage } from '../pages';
+import { MainPage, ComicsPage, SinglePage } from '../pages';
 import AppHeader from "../appHeader/AppHeader";
 import Spinner from '../spinner/Spinner';
+import SingleCharacterLayout from '../pages/singleCharacterLayout/SingleCharacterLayout';
+import SingleComicLayout from '../pages/singleComicLayout/SingleComicLayout';
 
 const Page404 = lazy(() => import('../pages/404'));
 
@@ -19,7 +21,10 @@ const App = () => {
                         <Routes>
                             <Route path="/" element={<MainPage />} />
                             <Route path="/comics" element={<ComicsPage />} />
-                            <Route path="/comics/:comicId" element={<SingleComicPage />} />
+
+                            <Route path="/comics/:id" element={<SinglePage Component={SingleComicLayout} dataType='comic'/>} />
+                            <Route path="/characters/:id" element={<SinglePage Component={SingleCharacterLayout} dataType='character'/>} />
+
                             <Route path="*" element={< Page404 />} />
                         </Routes>
                     </Suspense>
