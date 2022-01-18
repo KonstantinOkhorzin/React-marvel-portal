331c4933535b39919e196a32b655f6d1034459e3
Konstantin Okhorzin <konstantinokhorzin@gmail.com>
2022-01-18 17:00:30 +0200

    загрузка рандомного персонажа

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -1,22 +1,51 @@
+import { useState, useEffect } from 'react';
+
+import MarvelService from '../../services/MarvelService';
 import './randomChar.scss';
-import thor from '../../resources/img/thor.jpeg';
 import mjolnir from '../../resources/img/mjolnir.png';
 
 const RandomChar = () => {
+
+    const [char, setChar] = useState({});
+
+
+    const marvelService = new MarvelService();
+
+    useEffect(() => {
+        updateChar()
+    }, [])
+
+    //Функция для записи персонажа в состояние когда он загрузился
+    const onChatLoaded = (char) => {
+        setChar(char)
+    }
+
+    //Функция для обновления персонажа
+    const updateChar = () => {
+        const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);//рандомное id персонажа
+        marvelService
+            .getCharacter(id)
+            .then(onChatLoaded)
+    }
+
+
+
+    const { name, description, thumbnail, homepage, wiki } = char;
+
     return (
         <div className="randomchar">
             <div className="randomchar__block">
-                <img src={thor} alt="Random character" className="randomchar__img"/>
+                <img src={thumbnail} alt="Random character" className="randomchar__img" />
                 <div className="randomchar__info">
-                    <p className="randomchar__name">Thor</p>
+                    <p className="randomchar__name">{name}</p>
                     <p className="randomchar__descr">
-                        As the Norse God of thunder and lightning, Thor wields one of the greatest weapons ever made, the enchanted hammer Mjolnir. While others have described Thor as an over-muscled, oafish imbecile, he's quite smart and compassionate...
+                        {description}
                     </p>
                     <div className="randomchar__btns">
-                        <a href="#" className="button button__main">
+                        <a href={homepage} className="button button__main">
                             <div className="inner">homepage</div>
                         </a>
-                        <a href="#" className="button button__secondary">
+                        <a href={wiki} className="button button__secondary">
                             <div className="inner">Wiki</div>
                         </a>
                     </div>
@@ -24,7 +53,7 @@ const RandomChar = () => {
             </div>
             <div className="randomchar__static">
                 <p className="randomchar__title">
-                    Random character for today!<br/>
+                    Random character for today!<br />
                     Do you want to get to know him better?
                 </p>
                 <p className="randomchar__title">
@@ -33,10 +62,11 @@ const RandomChar = () => {
                 <button className="button button__main">
                     <div className="inner">try it</div>
                 </button>
-                <img src={mjolnir} alt="mjolnir" className="randomchar__decoration"/>
+                <img src={mjolnir} alt="mjolnir" className="randomchar__decoration" />
             </div>
         </div>
     )
+
 }
 
 export default RandomChar;
