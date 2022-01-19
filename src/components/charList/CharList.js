d1b7f1fe40240313627ef18cc47726a13bfb4673
Konstantin Okhorzin <konstantinokhorzin@gmail.com>
2022-01-19 10:56:24 +0200

    вывод списка персонажей на страницу

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,52 +1,80 @@
+import { useEffect, useState } from 'react';
+
+import Spinner from '../spinner/Spinner';
+import ErrorMessage from '../errorMessage/ErrorMessage';
+import MarvelService from '../../services/MarvelService';
+
 import './charList.scss';
-import abyss from '../../resources/img/abyss.jpg';
 
 const CharList = () => {
-    return (
-        <div className="char__list">
+
+    const [charList, setCharList] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(false);
+
+    const marvelService = new MarvelService();
+
+    //Функция для записи персонажей в состояние когда они загрузилися
+    const onCharListLoaded = (charList) => {
+        setCharList(charList)
+        setLoading(false)
+    }
+
+    //Функция для установки ошибки
+    const onError = () => {
+            setError(true)
+            setLoading(false)
+    }
+
+    useEffect(() => {
+        marvelService.getAllCharacters()
+        .then(onCharListLoaded)
+        .catch(onError)
+    }, [])
+
+    //Этот функция создана для оптимизации, чтобы не помещать такую конструкцию в return
+    const renderItems = (arr) => {
+        const items =  arr.map((item) => {
+
+            let imgStyle = {'objectFit' : 'cover'};
+            if (item.thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
+                imgStyle = {'objectFit' : 'unset'};
+            }
+            
+            return (
+                <li 
+                    className="char__item"
+                    key={item.id}>
+                        <img src={item.thumbnail} alt={item.name} style={imgStyle}/>
+                        <div className="char__name">{item.name}</div>
+                </li>
+            )
+        });
+
+        // А эта конструкция вынесена для центровки спиннера/ошибки
+        return (
             <ul className="char__grid">
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item char__item_selected">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
+                {items}
             </ul>
-            <button className="button button__main button__long">
-                <div className="inner">load more</div>
-            </button>
-        </div>
-    )
+        )
+    }
+
+        const items = renderItems(charList);
+
+        const errorMessage = error ? <ErrorMessage/> : null;
+        const spinner = loading ? <Spinner/> : null;
+        const content = !(loading || error) ? items : null;
+
+        return (
+            <div className="char__list">
+                {errorMessage}
+                {spinner}
+                {content}
+                <button className="button button__main button__long">
+                    <div className="inner">load more</div>
+                </button>
+            </div>
+        )
 }
 
 export default CharList;
