63145f59d5ebfc63c8c2f6750548ea375725a559
Konstantin Okhorzin <konstantinokhorzin@gmail.com>
2022-01-28 16:18:00 +0200

    ErrorBoundary

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -4,6 +4,7 @@ import AppHeader from "../appHeader/AppHeader";
 import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
 import CharInfo from "../charInfo/CharInfo";
+import ErrorBoundary from '../errorBoundary/ErrorBoundary';
 
 import decoration from '../../resources/img/vision.png';
 
@@ -18,14 +19,20 @@ const App = () => {
 
     return (
         <div className="app">
-            <AppHeader/>
+            <AppHeader />
             <main>
-                <RandomChar/>
+                <ErrorBoundary>
+                    <RandomChar />
+                </ErrorBoundary>
                 <div className="char__content">
-                    <CharList onCharSelected={onCharSelected}/>
-                    <CharInfo charId={selectedChar}/>
+                    <ErrorBoundary>
+                        <CharList onCharSelected={onCharSelected} />
+                    </ErrorBoundary>
+                    <ErrorBoundary>
+                        <CharInfo charId={selectedChar} />
+                    </ErrorBoundary>
                 </div>
-                <img className="bg-decoration" src={decoration} alt="vision"/>
+                <img className="bg-decoration" src={decoration} alt="vision" />
             </main>
         </div>
     )
